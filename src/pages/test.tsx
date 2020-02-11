7dfb24fdb5bf8ba6d8affa72be4f7160222fe2dc
makame <makame@mail.ru>
2020-02-11 18:55:37 +0100

    fix

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { Clock } from '../design-system/Clock';
+import { Clock } from '../design-system/clock';
 
 export default observer(() => {
   return (
